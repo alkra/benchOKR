b9198c8328b47798c06e22fb5177be347875b779
Niamien David <niamiendavid@gmail.com>
2015-05-08 12:33:04 +0200

    Mise à jour de code pour la construction de l'Octree Fichier Main David

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,54 @@
  * 77455 MARNE-LA-VALLÉE CEDEX 2
  * FRANCE */
 
+#include <QCoreApplication>
 #include <iostream>
 #include<string>
 using namespace std;
 
 #include "../include/Point.h"
-#include "../include/KdTree.h"
+#include "../include/Octree.h"
+#include "../include/NoeudOctree.h"
+#include"../include/Point3D.h"
+#include"../include/Fichier.h"
+#include"..//include/Arbre.h"
+#include <QDebug>
+#include<QString>
 
-
-int main(int argc, char** argv)
+int main()
 {
-    KdTree monKd;
+    //Fichier util;
+    // Ouverture du fichier
+
+    //util.ouvrir("C:/code04Mai2015/constructionOctree4Mai/araignee.ply", QIODevice::ReadOnly | QIODevice::Text);
+    //util.ouvrir("E:/Git/ProjetDeveloppement/jeuTest/Salamandre.ply", QIODevice::ReadOnly | QIODevice::Text);
+
+
+
+
+
+    // renvoyer la liste des points
+    //Point **points = util.getPoints();
+    // compter le nombre de points
+
+    //int nbrePts = util.getNbPoints();
+
+    //qDebug()<<nbrePts;
+
+//    QString ligneLue;
+
+//    ligneLue=points.at(10)->toQString();
+
+//    qDebug()<< ligneLue;
+
+
+    Octree monArbre;
+    QString chemin="C:/code04Mai2015/constructionOctree4Mai/araignee.ply";
+
+     monArbre.construire(chemin,500,4,0);
+
+
+    //test.construire(points,nbrePts,500,4,b,0);
+    return 0;
 }
+
